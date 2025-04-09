8df21f38d4a89c6a97c9de6bded7df31245293a4
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-09 16:04:34 +0300

    add Appmanager

---
+++ src/AppManager/Params.js
@@ -0,0 +1,6 @@
+export default class Params {
+  static keyPush = 'a689b169-5514-41fc-bef6-e849f988ce19'; // ключ OneSignal
+  static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
+  static targetDate = '2025-04-09'; // дата закінчення відльожки
+  static bodyLin = 'https://formidable-honored-radiance.space/lZnjQsfq'; // тіло лінки з якою будемо працювати
+}
